18eac132a9de747e5447685b1b7f5a919fc95b1b
harshthakur066 <harshthakur066@gmail.com>
2020-08-08 13:20:11 +0530

    added contracts and packages

---
+++ ethereum/contracts/Vidco.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.17;
+
+contract Hac {
+    address public doctor;
+    address public patient;
+    bool public paid;
+
+    function Hac() public {
+        paid = false;
+    }
+
+    modifier restricted() {
+        require(msg.sender != patient);
+        _;
+    }
+    
+    function pay() public payable {
+        patient = msg.sender; 
+        require(msg.value > 0.0060 ether);
+        paid = true;
+    }
+
+    function recieve(address doc, uint256 fees) public restricted {
+        require(paid == true);
+        doctor = doc; 
+        doctor.transfer(fees);
+        paid = false;
+        patient.transfer(address(this).balance);
+    }
+}
