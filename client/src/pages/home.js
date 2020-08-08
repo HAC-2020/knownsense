86cfc2ca9b1c8256f0ad7161e38b2fd0aad58536
shagunattri <shagunattri17@gmail.com>
2020-08-08 14:53:22 +0530

    add navbar and fix CSS

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -10,13 +10,13 @@ export default class home extends Component {
     }
     return (
       <div className="mt-4 text-center">
-        <p>Welcome to Video Call App</p>
+        <p>Welcome to <strong>Vidco</strong></p>
 
         <div className="mt-4">
           <a className="btn btn-primary" href={'/joincall/' + uuidv4()}>Create new Meeting</a>
         </div>
         <div className="mt-4">
-          <input type="text" id="meetid" className="form-control" placeholder="Enter Meeting ID" />
+          <input type="text" id="meetid" className="form-control" placeholder="Enter Meeting ID" style={{width: "350px", margin: "0 auto",}}  />
           <button onClick={handleJoin} className="btn btn-primary">Join Meeting</button>
         </div>
       </div >
